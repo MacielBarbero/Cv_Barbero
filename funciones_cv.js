037ef7274a89b768b1ea57e10cfa8a1c2f7dd55c
MacielBarbero <122734111+MacielBarbero@users.noreply.github.com>
2023-01-15 11:31:26 -0300

    Add files via upload

---
+++ funciones_cv.js
@@ -0,0 +1,26 @@
+
+document.getElementById("curso0").style.display = "none";
+document.getElementById("curso1").style.display = "none";
+document.getElementById("curso2").style.display = "none";
+
+
+
+
+document.getElementById("mostrar_contenido0").addEventListener("click", function() {
+     
+    document.getElementById("curso0").style.display = "block";
+
+});	
+
+document.getElementById("mostrar_contenido1").addEventListener("click", function() {
+     
+    document.getElementById("curso1").style.display = "block";
+
+});	
+
+document.getElementById("mostrar_contenido2").addEventListener("click", function() {
+     
+    document.getElementById("curso2").style.display = "block";
+
+});	
+
